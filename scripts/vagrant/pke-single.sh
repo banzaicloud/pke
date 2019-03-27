f4128754d8cde8eef64cfe315992bf271631efb4
Tamás Ecsédi <ecsy.argh@gmail.com>
2019-03-27 17:22:23 +0100

    Single command to run PKE with vagrant. fixes #10

---
+++ scripts/vagrant/pke-single.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+/usr/local/bin/pke version -o yaml || ( \
+    curl -v https://banzaicloud.com/downloads/pke/pke-0.2.3 -o /usr/local/bin/pke && \
+    chmod +x /usr/local/bin/pke
+)
+
+systemctl is-active kubelet || ( \
+    /usr/local/bin/pke install single && \
+    mkdir -p $HOME/.kube && \
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
+    chown $(id -u):$(id -g) $HOME/.kube/config
+)
