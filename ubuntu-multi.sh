f908b9b66c2f52a152a0d11ac40b2476b0a90537
Tamás Ecsédi <ecsy.argh@gmail.com>
2019-09-30 17:18:44 +0200

    Fix ip address in ubuntu script

--- ubuntu-multi.sh
+++ ubuntu-multi.sh
@@ -12,7 +12,7 @@ KUBERNETES_VERSION="${1:-v1.16.0}"
 echo ""
 echo "= ubuntu1 ========================================================================"
 vagrant up ubuntu1
-vagrant ssh ubuntu1 -c "sudo /scripts/pke-multi-master1.sh '$KUBERNETES_VERSION' '192.168.64.12:6443'"
+vagrant ssh ubuntu1 -c "sudo /scripts/pke-multi-master1.sh '$KUBERNETES_VERSION' '192.168.64.21:6443'"
 vagrant ssh ubuntu1 -c 'sudo cat /etc/kubernetes/admin.conf' > pke-multi-config.yaml
 vagrant ssh ubuntu1 -c "sudo /banzaicloud/pke token list -o json" > build/token.json
 
@@ -26,19 +26,19 @@ echo "Using '$TOKEN' and '$CERTHASH' to join other nodes to the cluster"
 echo ""
 echo "= ubuntu2 ========================================================================"
 vagrant up ubuntu2
-vagrant ssh ubuntu2 -c "sudo /scripts/pke-multi-mastern.sh '$KUBERNETES_VERSION' '192.168.64.12:6443' '$TOKEN' '$CERTHASH' 192.168.64.12"
+vagrant ssh ubuntu2 -c "sudo /scripts/pke-multi-mastern.sh '$KUBERNETES_VERSION' '192.168.64.21:6443' '$TOKEN' '$CERTHASH' 192.168.64.22"
 
 # install third master node
 echo ""
 echo "= ubuntu3 ========================================================================"
 vagrant up ubuntu3
-vagrant ssh ubuntu3 -c "sudo /scripts/pke-multi-mastern.sh '$KUBERNETES_VERSION' '192.168.64.12:6443' '$TOKEN' '$CERTHASH' 192.168.64.13"
+vagrant ssh ubuntu3 -c "sudo /scripts/pke-multi-mastern.sh '$KUBERNETES_VERSION' '192.168.64.21:6443' '$TOKEN' '$CERTHASH' 192.168.64.23"
 
 # install worker node
 echo ""
 echo "= ubuntu4 ========================================================================"
 vagrant up ubuntu4
-vagrant ssh ubuntu4 -c "sudo /scripts/pke-multi-worker.sh '$KUBERNETES_VERSION' '192.168.64.12:6443' '$TOKEN' '$CERTHASH'"
+vagrant ssh ubuntu4 -c "sudo /scripts/pke-multi-worker.sh '$KUBERNETES_VERSION' '192.168.64.21:6443' '$TOKEN' '$CERTHASH'"
 
 export KUBECONFIG=$PWD/pke-multi-config.yaml
 
