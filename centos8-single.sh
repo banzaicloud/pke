972bdb0e2dcb5ac5b00a6e949d0462c9c266c8fb
Peter Balogh <p.balogh.sa@gmail.com>
2020-10-07 10:53:48 +0200

    update k8s version in case of CentOS8 installer script
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- centos8-single.sh
+++ centos8-single.sh
@@ -3,7 +3,7 @@
 # build latest pke tool
 GOOS=linux make pke
 
-KUBERNETES_VERSION="${1:-v1.18.9}"
+KUBERNETES_VERSION="${1:-v1.19.2}"
 
 export VAGRANT_VAGRANTFILE=Vagrantfile-centos8
 vagrant up centos1
