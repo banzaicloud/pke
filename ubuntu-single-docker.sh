2a8db4653ddf98db76a6404fd29f9832316427e6
Peter Balogh <p.balogh.sa@gmail.com>
2020-12-20 13:30:05 +0100

    fix: docker installation in case of ubuntu focal
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- ubuntu-single-docker.sh
+++ ubuntu-single-docker.sh
@@ -3,11 +3,12 @@
 # build latest pke tool
 GOOS=linux make pke
 
-KUBERNETES_VERSION="${1:-v1.18.14}"
+KUBERNETES_VERSION="${2:-v1.18.14}"
+UBUNTU_VERSION=${1:-focal}
 
-vagrant up ubuntu-docker
-vagrant ssh ubuntu-docker -c "sudo /scripts/pke-single.sh '$KUBERNETES_VERSION' '192.168.64.30:6443' docker"
-vagrant ssh ubuntu-docker -c 'sudo cat /etc/kubernetes/admin.conf' > pke-single-config.yaml
+vagrant up ubuntu-docker-${UBUNTU_VERSION}
+vagrant ssh ubuntu-docker-${UBUNTU_VERSION} -c "sudo /scripts/pke-single.sh '$KUBERNETES_VERSION' '192.168.64.30:6443' docker"
+vagrant ssh ubuntu-docker-${UBUNTU_VERSION} -c 'sudo cat /etc/kubernetes/admin.conf' > pke-single-config.yaml
 
 export KUBECONFIG=$PWD/pke-single-config.yaml
 
@@ -17,4 +18,4 @@ echo "- from your host machine accessing the cluster via kubectl. Please run:"
 echo "export KUBECONFIG=$PWD/pke-single-config.yaml"
 echo ""
 echo "- or starting a shell in the virtual machine. Please run:"
-echo "vagrant ssh ubuntu-docker -c 'sudo -s'"
+echo "vagrant ssh ubuntu-docker-${UBUNTU_VERSION} -c 'sudo -s'"
