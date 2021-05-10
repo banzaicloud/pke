ac13aeb16761c5cf721844d7b0740a2257679ac4
Peter Balogh <p.balogh.sa@gmail.com>
2021-05-10 10:54:24 +0200

    workaround for vagrant test upgrade
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- centos8-multi-upgrade.sh
+++ centos8-multi-upgrade.sh
@@ -10,6 +10,9 @@ echo ""
 echo "= centos1 ========================================================================"
 vagrant ssh centos1 -c "sudo /banzaicloud/pke upgrade master --kubernetes-version='$KUBERNETES_VERSION'"
 
+# waiting 10 seconds because of apiserver
+sleep 10
+
 # upgrade second master node
 echo ""
 echo "= centos2 ========================================================================"
