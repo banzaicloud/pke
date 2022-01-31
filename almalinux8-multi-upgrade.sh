d98508c9d931d129ac16dc9b71eca7be94fb63df
Peter Balogh <p.balogh.sa@gmail.com>
2022-01-31 09:58:05 +0100

    chore!: drop support for ubuntu 18.04 and centos8 and add support for almalinux8
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

---
+++ almalinux8-multi-upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# build latest pke tool
+GOOS=linux make pke
+
+KUBERNETES_VERSION="${1:-v1.21.0}"
+
+# upgrade first master node
+echo ""
+echo "= almalinux1 ========================================================================"
+vagrant ssh almalinux1 -c "sudo /banzaicloud/pke upgrade master --kubernetes-version='$KUBERNETES_VERSION'"
+
+# waiting 10 seconds because of apiserver
+sleep 10
+
+# upgrade second master node
+echo ""
+echo "= almalinux2 ========================================================================"
+vagrant ssh almalinux2 -c "sudo /banzaicloud/pke upgrade master --kubernetes-version='$KUBERNETES_VERSION' --kubernetes-additional-control-plane"
+
+# upgrade third master node
+echo ""
+echo "= almalinux3 ========================================================================"
+vagrant ssh almalinux3 -c "sudo /banzaicloud/pke upgrade master --kubernetes-version='$KUBERNETES_VERSION' --kubernetes-additional-control-plane"
+
+# upgrade worker node
+echo ""
+echo "= almalinux4 ========================================================================"
+vagrant ssh almalinux4 -c "sudo /banzaicloud/pke upgrade worker --kubernetes-version='$KUBERNETES_VERSION'"
